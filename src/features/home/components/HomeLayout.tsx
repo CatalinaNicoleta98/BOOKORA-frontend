a3dc41f3585c29938b55d7acc0a6cc321d2b3092
Catalina Vrinceanu <cnv98@yahoo.com>
2026-03-30 14:16:47 +0200

    home layout update

--- src/features/home/components/HomeLayout.tsx
+++ src/features/home/components/HomeLayout.tsx
@@ -9,12 +9,21 @@ type HomeLayoutProps = {
 
 const HomeLayout = ({ data }: HomeLayoutProps) => {
     return (
-        <main className="grid grid-cols-12 gap-6 p-6">
-            <ReadingSidebar data={data} />
+        <main className="mx-auto w-full max-w-[1400px] grid grid-cols-12 gap-6 px-4 py-6 sm:px-6 lg:px-10 lg:py-10">
+            {/* Left sidebar */}
+            <div className="order-2 col-span-12 md:col-span-6 xl:order-1 xl:col-span-3">
+                <ReadingSidebar data={data} />
+            </div>
 
-            <ActivityFeed data={data} />
+            {/* Center feed */}
+            <div className="order-1 col-span-12 xl:order-2 xl:col-span-6">
+                <ActivityFeed data={data} />
+            </div>
 
-            <RecommendationsSidebar data={data} />
+            {/* Right sidebar */}
+            <div className="order-3 col-span-12 md:col-span-6 xl:col-span-3">
+                <RecommendationsSidebar data={data} />
+            </div>
         </main>
     );
 };

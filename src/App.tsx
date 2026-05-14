2d1f2cc033c2ba630bdceb298496a5974875e12a
Catalina Vrinceanu <cnv98@yahoo.com>
2026-05-14 14:16:18 +0200

    fix:scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./app/router/AppRouter";
+import ScrollToTop from "./app/router/ScrollToTop";
 
 const App = () => {
     return (
         <BrowserRouter>
+            <ScrollToTop />
             <div className="min-h-screen">
                 <AppRouter />
             </div>
